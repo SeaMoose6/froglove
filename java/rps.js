60796bf3bcdc4c0358744a8a36672db4cff4388c
SeaMoose6 <tophtoph05@gmail.com>
2023-11-06 12:44:02 -0500

    rps game score

--- java/rps.js
+++ java/rps.js
@@ -6,9 +6,15 @@ const scissorsBtn = document.getElementById('scissors')
 const results = document.querySelector('.result')
 const userImg = document.querySelector('.user-img')
 const compImg = document.querySelector('.comp-img')
+const userWins = document.querySelector(".user-score")
+const compWins = document.querySelector(".comp-score")
+const resetBtn = document.getElementById('reset')
+
 
 const choices = ['rock', 'paper', 'scissors']
 let comp
+let comp_score = 0
+let user_score = 0
 // function rockBtnClick() {
 //     changes text of the result object
 //     results.innerText = 'rock clicked'
@@ -20,15 +26,26 @@ rockBtn.addEventListener('click', () => {
     userImg.src = "../image/" + rockBtn.id + ".png"
     // can style result with result.style.color = "red"
     compChoice()
-    results.innerText = comp
+    winLose(rockBtn.id)
+    // console.log(comp_score)
 })
 scissorsBtn.addEventListener('click', () => {
     userImg.src = "../image/" + scissorsBtn.id + ".png"
     compChoice()
+    winLose(scissorsBtn.id)
 })
 paperBtn.addEventListener('click', () => {
     userImg.src = "../image/" + paperBtn.id + ".png"
     compChoice()
+    winLose(paperBtn.id)
+})
+resetBtn.addEventListener('click', () => {
+    console.log(comp_score, user_score)
+    comp_score = 0
+    user_score = 0
+    compWins.innerText = comp_score.toString()
+    userWins.innerText = user_score.toString()
+    console.log(comp_score, user_score)
 })
 
 function compChoice () {
@@ -38,5 +55,34 @@ function compChoice () {
 }
 
 function winLose (userChoice) {
-    
+    if (userChoice == "rock" && comp == "paper"){
+        results.innerText = 'Computer Wins'
+        comp_score = comp_score+1
+        compWins.innerText = comp_score.toString()
+    } else if (userChoice == "scissors" && comp == "rock") {
+        results.innerText = 'Computer Wins'
+        comp_score = comp_score+1
+        compWins.innerText = comp_score.toString()
+    } else if (userChoice == "paper" && comp == "scissors") {
+        results.innerText = 'Computer Wins'
+        comp_score = comp_score+1
+        compWins.innerText = comp_score.toString()
+    } else if (userChoice == "rock" && comp == "scissors"){
+        results.innerText = 'User Wins'
+        user_score = user_score+1
+        userWins.innerText = user_score.toString()
+    } else if (userChoice == "scissors" && comp == "paper") {
+        results.innerText = 'User Wins'
+        user_score = user_score+1
+        userWins.innerText = user_score.toString()
+    } else if (userChoice == "paper" && comp == "rock") {
+        results.innerText = 'User Wins'
+        user_score = user_score+1
+        userWins.innerText = user_score.toString()
+    } else {
+        results.innerText = "Tie Game"
+    }
+    if (user_score == 3){
+        results.inner
+    }
 }
