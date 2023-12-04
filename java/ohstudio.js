322afb9f5b290e13dae98524b5079d4ac48a3745
SeaMoose6 <tophtoph05@gmail.com>
2023-12-04 12:44:48 -0500

    java fixed

--- java/ohstudio.js
+++ java/ohstudio.js
@@ -1,6 +1,11 @@
-const image = document.getElementById('image')
+const images = document.querySelectorAll("#image")
+images.forEach((image) => {
+    console.log(image)
 
-image.addEventListener('mouseover', () => {
-    
-    console.log("true")
+    image.addEventListener('mouseover', (e) => {
+        image.style.filter = "blur(8Px)"
+    })
+    image.addEventListener('mouseout', (e) => {
+        image.style.filter = "none"
+    })
 })
