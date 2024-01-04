292caf179b4a7973b120a19f5615ce6a3d263d2e
SeaMoose6 <tophtoph05@gmail.com>
2024-01-04 12:44:11 -0500

    framework for FAQ

---
+++ java/monogram.js
@@ -0,0 +1,17 @@
+const productList = document.querySelectorAll('.product')
+const priceList = document.querySelectorAll('.item_price')
+const shopBtnList = document.querySelectorAll('.shop-btn')
+
+
+productList.forEach(product => {
+    product.addEventListener('mouseover', () =>{
+        let index = product.id
+        shopBtnList[index].style.display='block'
+        priceList[index].style.display='none'
+    })
+    product.addEventListener('mouseout', () =>{
+        let index = product.id
+        shopBtnList[index].style.display='none'
+        priceList[index].style.display='block'
+    })
+});
